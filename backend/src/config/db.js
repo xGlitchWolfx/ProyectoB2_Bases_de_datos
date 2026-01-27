5629af79364b6b23f9592c510230a096ab641be6
xGlitchWolfx <uspwolf2563@gmail.com>
2026-01-27 17:45:37 -0500

    fixeo

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
